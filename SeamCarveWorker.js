3e47b0119f7f29c0d2efad84c4bcfaaf16bc2dab
Jamal Carvalho <jamal.a.carvalho@gmail.com>
2015-09-15 18:02:08 -0400

    bug fix

---
+++ SeamCarveWorker.js
@@ -0,0 +1,27 @@
+'use strict';
+
+class SeamCarveWorker {
+	constructor() {
+		this.worker = new Worker('SeamCarve.js');
+	}
+
+	adjust(image, newWidth, callback) {
+		this.worker.postMessage({
+			action: 'adjust',
+			payload: {
+				image: image,
+				width: newWidth
+			}
+		});
+
+		this.worker.onmessage = function(e) {
+			if (e.data.action === 'update') {
+				callback(e.data.payload);
+			}
+
+			if (e.data.action === 'error') {
+				window.console.log('error');
+			}
+		};
+	}
+}
